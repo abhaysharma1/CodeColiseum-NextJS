309db088509f4f2657eb86f68ac4500f619347ae
Abhay Sharma <abhaysharma.mrt@gmail.com>
2026-02-21 17:37:57 +0530

    Fixed Backend URL Auth Client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,11 +2,12 @@ import "dotenv/config"
 import { createAuthClient } from "better-auth/react"
 import { inferAdditionalFields } from "better-auth/client/plugins"
 import type { auth } from "./auth"
+import { getBackendURL } from "@/utils/utilities"
 
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "http://localhost:5000",
+    baseURL: getBackendURL(),
     plugins: [
         inferAdditionalFields<typeof auth>()
     ]
