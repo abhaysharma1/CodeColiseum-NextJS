96b9b1a3e607d805e1a285e89661244e668c52d6
Abhay Sharma <abhaysharma.mrt@gmail.com>
2026-04-03 16:00:41 +0530

    Updated backend URI

--- utils/utilities.ts
+++ utils/utilities.ts
@@ -1,5 +1,5 @@
 
 export function getBackendURL() {
-  return process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "https://api.codecoliseum.in";
+  return process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "internal-codecoliseum-backend-alb-745028571.ap-south-1.elb.amazonaws.com";
 }
 
