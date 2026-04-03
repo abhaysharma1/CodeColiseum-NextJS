661e903fa428de397f85c0650fd9abbe3589cb20
Abhay Sharma <abhaysharma.mrt@gmail.com>
2026-04-03 16:07:47 +0530

    again fixed backend uri to use aws backend

--- utils/utilities.ts
+++ utils/utilities.ts
@@ -1,5 +1,5 @@
 
 export function getBackendURL() {
-  return process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "internal-codecoliseum-backend-alb-745028571.ap-south-1.elb.amazonaws.com";
+  return process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "http://internal-codecoliseum-backend-alb-745028571.ap-south-1.elb.amazonaws.com";
 }
 
